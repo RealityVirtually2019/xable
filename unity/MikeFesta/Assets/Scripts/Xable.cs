fba8ab0f4c48e0e8c8c9f9075aa8dbfe0503e7c8
Mike Festa <mike@festatech.com>
2019-01-19 17:28:05 -0500

    created scripts that can be put in a prefab

---
+++ unity/MikeFesta/Assets/Scripts/Xable.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xable : MonoBehaviour
+{
+
+    public string AltText;
+    public Texture HighContrast;
+    //ColorPallet;
+    //AudioFile;
+    //Haptics;
+    //ClickAction - enlarge, animate
+
+
+    private ADASettings adaSettings;
+    private ADAInput adaInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.adaSettings = Object.FindObjectOfType<ADASettings>();
+        this.adaInput = Object.FindObjectOfType<ADAInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Based on the bool flags in the ADA settings, perform different actions on this game object
+       if (this.adaSettings.LowVision)
+       {
+          // TODO: Change this to an event listener based model - this is just for quick testing
+          if (this.adaInput.SelectAction() && this.InFocus())
+          {
+              this.EnlargeScale();
+          }
+          else if (this.adaInput.DeselectAction())
+          {
+              this.RestoreScale();
+          }
+       }
+    }
+
+    bool InFocus()
+    {
+        // TODO: Come up with a universal way to have one Xable object in focus at a time
+        // This should be similar to the TabFocus HTML attribute in HTML
+        return true;
+    }
+
+    void EnlargeScale()
+    {
+        this.transform.localScale = new Vector3(this.adaSettings.EnlargeScale,this.adaSettings.EnlargeScale,this.adaSettings.EnlargeScale);
+        // TODO: Center in front of the user
+    }
+
+    void RestoreScale()
+    {
+        this.transform.localScale = new Vector3(1,1,1);
+    }
+}
