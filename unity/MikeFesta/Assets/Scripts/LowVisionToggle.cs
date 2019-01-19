6ace13aca006d98b29d7b02bf696dde12031a82b
Mike Festa <mike@festatech.com>
2019-01-19 01:09:51 -0500

    low vision test ready

--- unity/MikeFesta/Assets/Scripts/LowVisionToggle.cs
+++ unity/MikeFesta/Assets/Scripts/LowVisionToggle.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class LowVisionToggle : MonoBehaviour
 {
 
-    public enum VisionLevel {Full, Blur_1x_10, Blur_2x_10, Blur_3x_10};
+    public enum VisionLevel {
+        Full, 
+        Low1,
+        Low2,
+        Low3,
+        Low4,
+        Low5,
+        Low6
+    };
 
     public VisionLevel initialVisionLevel;
     public Material skybox;
     public Texture lowVision1Texture;
     public Texture lowVision2Texture;
     public Texture lowVision3Texture;
+    public Texture lowVision4Texture;
+    public Texture lowVision5Texture;
+    public Texture lowVision6Texture;
     public Texture fullTexture;
 
     private VisionLevel currentState;
@@ -31,15 +42,27 @@ public class LowVisionToggle : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            SetState(VisionLevel.Blur_1x_10);
+            SetState(VisionLevel.Low1);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            SetState(VisionLevel.Blur_2x_10);
+            SetState(VisionLevel.Low2);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            SetState(VisionLevel.Blur_3x_10);
+            SetState(VisionLevel.Low3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetState(VisionLevel.Low4);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SetState(VisionLevel.Low5);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            SetState(VisionLevel.Low6);
         }
     }
 
@@ -47,17 +70,26 @@ public class LowVisionToggle : MonoBehaviour
     void SetState(VisionLevel newState) {
         this.currentState = newState;
         switch(newState) {
-            case VisionLevel.Blur_1x_10:
-                this.skybox.SetTexture("_Tex", this.lowVision1Texture);
-                break;
-            case VisionLevel.Blur_2x_10:
-                this.skybox.SetTexture("_Tex", this.lowVision2Texture);
-                break;
-            case VisionLevel.Blur_3x_10:
-                this.skybox.SetTexture("_Tex", this.lowVision3Texture);
-                break;
             case VisionLevel.Full:
                 this.skybox.SetTexture("_Tex", this.fullTexture);
+                break;
+            case VisionLevel.Low1:
+                this.skybox.SetTexture("_Tex", this.lowVision1Texture);
+                break;
+            case VisionLevel.Low2:
+                this.skybox.SetTexture("_Tex", this.lowVision2Texture);
+                break;
+            case VisionLevel.Low3:
+                this.skybox.SetTexture("_Tex", this.lowVision3Texture);
+                break;
+            case VisionLevel.Low4:
+                this.skybox.SetTexture("_Tex", this.lowVision4Texture);
+                break;
+            case VisionLevel.Low5:
+                this.skybox.SetTexture("_Tex", this.lowVision5Texture);
+                break;
+            case VisionLevel.Low6:
+                this.skybox.SetTexture("_Tex", this.lowVision6Texture);
                 break;
         }
     }
