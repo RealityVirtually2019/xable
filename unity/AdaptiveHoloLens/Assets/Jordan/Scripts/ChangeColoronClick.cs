3e30957c1279db41ad0cbb4ff3891761293ac389
Jordan Higgins <jhiggins@gmail.com>
2019-01-20 08:05:32 -0500

    Adding Xable icons, latest Unity Build
    Icons for Grow, Shrink, and Outline

---
+++ unity/AdaptiveHoloLens/Assets/Jordan/Scripts/ChangeColoronClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class ChangeColoronClick : MonoBehaviour, IInputClickHandler {
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+
+        if (this.GetComponent<MeshRenderer>().material.color == Color.green)
+        {
+            this.GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+        else
+        {
+            this.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
